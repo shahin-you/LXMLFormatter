fc679b9ddf328c5e8f0267a6e477ad658af19e3d
shahin-youssefi-wisi <shahin.youssefi@wisi.tv>
2025-07-30 15:40:35 -0700

    program/init added main.cpp
    This is the initial setup of the main entry point. No logic yet, but it's where the app will start.

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/*
+    * Large XML Formatter
+    * Version: 0.0.1
+    * Author: Shahin Youssefi
+    * License: MIT
+    * Date: 2025-07-30
+*/
+
+#include "BufferedInputStream.h"
+#include <iostream>
+
+int main() {
+    std::cout << "Large XML Formatter (ver 0.0.1)" << std::endl;
+    return 0;
+}
